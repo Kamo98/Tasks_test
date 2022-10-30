fb9ccf18a1be1d3aa3f42aa3152b181e44460665
Kamo <avanesjan.k@gmail.com>
2022-10-30 18:46:44 +0700

    Prod env

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from starlette.config import Config
 
-config = Config(".env_dev")
+config = Config(".env")
 
 # URL для подключения к БД
 DATABASE_URL = config("DATABASE_URL", cast=str, default="")
